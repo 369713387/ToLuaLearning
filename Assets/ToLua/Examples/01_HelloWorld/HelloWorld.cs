378413cc8f80b581fd949ead8605ed30e5f00f12
369713387 <369713387@qq.com>
2019-01-24 17:28:54 +0800

    ToLua

---
+++ Assets/ToLua/Examples/01_HelloWorld/HelloWorld.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using LuaInterface;
+using System;
+
+public class HelloWorld : MonoBehaviour
+{
+    void Awake()
+    {
+        LuaState lua = new LuaState();
+        lua.Start();
+        string hello =
+            @"        
+                function max(num1, num2)
+
+                if (num1 > num2) then
+                result = num1;
+                else
+                    result = num2;
+                end
+
+                return result; 
+                end
+                -- 调用函数
+                print('两值比较最大值为 ',max(10,4))
+                print('两值比较最大值为 ', max(5, 6))
+            ";
+        
+        lua.DoString(hello, "HelloWorld.cs");
+        lua.CheckTop();
+        lua.Dispose();
+        lua = null;
+    }
+}
